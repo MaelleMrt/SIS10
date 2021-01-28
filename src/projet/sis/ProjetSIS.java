40f19f53d9210705506fe0620e4eb3c06bf8337c
Maëlle <maelle.mg@gmail.com>
2021-01-28 11:08:01 +0100

    initialisation

---
+++ src/projet/sis/ProjetSIS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.sis;
+
+import library.interfaces.Patient;
+
+/**
+ *
+ * @author Maelle
+ */
+public class ProjetSIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int id =2334;
+        String surname="Paul";
+        int classe=3;
+        Patient p=new Patient(id,surname, classe);
+    }
+    
+}
