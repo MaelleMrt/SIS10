27150c7a0f37516581b6c233f85845bbad889418
Maëlle <maelle.mg@gmail.com>
2021-02-05 16:23:24 +0100

    création infarces inf
    Format des logos

---
+++ src/Infirmiere/TestInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Infirmiere;
+
+/**
+ *
+ * @author Maelle
+ */
+public class TestInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       InfirmiereObservation init=new InfirmiereObservation();
+        init.setVisible(true);
+    }
+    
+}
